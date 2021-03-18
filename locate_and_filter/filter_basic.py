73e987a9acee33028d46832940a44fe3cef4c510
Alec Xu <Alec_Xu@hotmail.com>
2021-03-18 15:48:31 -0700

    basic filtering initial commit

---
+++ locate_and_filter/filter_basic.py
@@ -0,0 +1,180 @@
+# Create dataset
+# Alec Xu
+# Partial code from Samuel Berryman
+# Multi-scale Design Lab
+# University of British Columbia
+# 2020
+from argparse import ArgumentParser as arg_parser
+from binarize_otsu import binarize_otsu
+from collections import defaultdict
+import matplotlib.pyplot as plt
+import numpy as np
+import os, os.path
+import pickle
+from PIL import Image
+from scipy import ndimage as ndi
+from skimage import filters
+from skimage.feature import peak_local_max
+from skimage.morphology import disk
+import skimage.morphology as morph
+from skimage.segmentation import clear_border
+from skimage.segmentation import watershed
+import time
+import cv2
+
+# Do definitions of validity in function for now. Move to config file later
+def sample_valid(sample, box_size, failures, invalids):
+    img_bf = np.copy(sample[0])
+    img_mito = np.copy(sample[1])
+    
+    img_cd80 = np.copy(sample[2])
+    img_cd206 = np.copy(sample[3])
+    min_size = 30
+    
+
+    # Check if mitochondria stain is too dim
+    if np.percentile(img_mito, 99) - np.percentile(img_mito, 5) < 20:
+        failures["mito_dim"] += 1
+        invalids["mito_dim"].append(sample)
+        return False
+    
+    mask_mito = binarize_otsu(img_mito, min_size)
+    kernel = np.ones((3, 3), np.uint8)
+    mask_erode = cv2.erode(mask_mito.astype(np.uint8), kernel, iterations=2)
+
+    # Allow diagonal connections to count as one object
+    connectivity = np.array(np.ones((3,3)), dtype=np.bool)
+    _, num_objs = ndi.label(mask_erode, connectivity)
+    
+    # Number of cells in sample using mito stain
+    # After this, mask_mito should only have 0 = background 1 = mito location
+    if num_objs > 1:
+        failures["num_obj"] += 1
+        invalids["num_obj"].append(sample)
+        return False        
+
+    # Size of mito stain in range cell size
+    mito_size = np.sum(mask_mito) # num px
+    if mito_size < 130:
+        failures["mito_size_small"] += 1
+        invalids["mito_size_small"].append(sample)
+        return False
+
+    if mito_size > 4000:
+        failures["mito_size_large"] += 1
+        invalids["mito_size_large"].append(sample)
+        return False
+
+    if mito_size < 120:
+        failures["mito_size_small"] += 1
+        invalids["mito_size_small"].append(sample)
+        return False
+        
+    # Check if crop area is correct
+    if not img_mito.shape == (box_size, box_size):
+        failures["crop_size"] += 1
+        invalids["crop_size"].append(sample)
+        return False
+    
+    # Check if picture is blank
+    unique, _ = np.unique(img_mito, return_counts=True)
+    # Check for blank image (less than 40 unique pixel intensities is blank)
+    if len(unique) < 20:
+        failures["blank"] += 1
+        invalids["blank"].append(sample)
+        return False
+
+    mass_centre = ndi.measurements.center_of_mass(mask_mito)
+    if not (0.4*box_size < mass_centre[0] < 0.6*box_size) and \
+       not (0.4*box_size < mass_centre[1] < 0.6*box_size):
+        failures["uncentred"] += 1
+        invalids["uncentred"].append(sample)
+        return False
+    return True 
+    
+def main(raw_args=None):
+    parser = arg_parser(description="Create dataset from cell images in dir. \
+        \n Input requires a directory with images under directories with \
+        corresponding cell name")
+    parser.add_argument("path", action="store", type=str, \
+                        help="Source path for dir containing raw images.")
+    parser.add_argument("-s", action="store", type=int, \
+                        help="size of training image in px (default=96")
+    parser.add_argument("-o", action="store", type=str, \
+                        help="out path of dataset (default=current dir")
+    parser.add_argument("-i", dest="i", action="store_true", \
+                        help="output invalid samples instead of valid samples")
+
+    # Default optional args
+    parser.set_defaults(s=96, o=".\\data", p=False, i=False, b=False)
+    
+    args = parser.parse_args()
+    
+    print("filtering dataset with params...") 
+    print("Image size: " + str(args.s) + "px")
+    print("Input path: " + args.path)
+    print("Output path: " + args.o)
+    print("Output invalid samples instead: " + str(args.i))
+
+    if not args.i and os.path.isfile(args.o) and args.o != ".\\data": 
+        sys.exit("output file exists... exiting")
+    if args.i and os.path.isdir(args.o) and args.o != ".\\data":
+        sys.exit("output folder exists... exiting")
+
+    print("\nLoading pickle")
+    start_time = time.time()
+    
+    # Create dict of lists to hold each label type
+    #label_types = set(labels)
+    unfiltered_samples = None
+    filtered_samples = []
+    invalid_samples = defaultdict(list)
+    failures = defaultdict(int)
+
+    with open(args.path, 'rb') as handle:
+        unfiltered_samples = pickle.load(handle)
+
+    print("Scaling to 8-bit")
+    unfiltered_samples = [np.uint8(sample/np.max(sample)*255) for sample in unfiltered_samples]
+    
+    num_cells = len(unfiltered_samples)
+    print("# candidate cells: " + str(num_cells))
+
+    print("\nTesting cropped sample cell images")
+    
+    load_inc = int(num_cells / 100)
+    for i in range(num_cells):
+        sample = unfiltered_samples[i]
+        if(sample_valid(sample, args.s, failures, invalid_samples)):
+            filtered_samples.append(sample)
+        if i % load_inc == 0:
+            print(f'Filtering: {i}/{num_cells}', end="\r")
+    
+    print("\nDone filtering")
+    print("Samples valid: " + str(len(filtered_samples)) + "\n")
+    unfiltered_samples = np.stack(unfiltered_samples, axis=0)
+
+
+    print("Samples discarded:")
+    [print(key, ":", failures[key]) for key in failures]
+
+    print("\nSaving valid samples")
+
+    if len(filtered_samples) > 0:
+        if not args.i:
+            print("Saving at: " + args.o)
+            pickle.dump(filtered_samples, open( args.o, "wb" ))
+        else:
+            print("Saving at: " + args.o)
+            os.mkdir(args.o)
+            for key in failures.keys():
+                out_file = open( args.o + "\\" + key, "wb" )
+                pickle.dump(invalid_samples[key], out_file)
+
+    elapsed = time.time() - start_time
+    print("\nTotal time: " + str(elapsed))
+
+    print('\nDataset creation completed')
+
+if __name__=="__main__":
+    main()
