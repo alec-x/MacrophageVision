96b49bafdf539ad7af6a7815b09409ceafdb3c91
alec xu <alec_xu@hotmail.com>
2021-06-02 11:45:32 -0700

    implement additional unmaxpooled conv layers and switch from sigmoid output to raw out.

--- ML/macnet.py
+++ ML/macnet.py
@@ -1,29 +1,34 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from torch import sigmoid
 
 class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-
         self.conv1 = nn.Conv2d(2, 32, 3)
+        self.conv1a = nn.Conv2d(32, 32, 3)
         self.conv2 = nn.Conv2d(32, 64, 3)
+        self.conv2a = nn.Conv2d(64, 64, 3)
         self.conv3 = nn.Conv2d(64, 128, 3)
-        
-        size_post_pool = int((96 / 2 / 2 / 2) - 2)
-        self.fc1 = nn.Linear(128 * size_post_pool * size_post_pool, 256)
+        self.conv3a = nn.Conv2d(128, 128, 3)
+
+        self.fc1 = nn.Linear(8192, 256)
         self.fc2 = nn.Linear(256, 256)
         self.fc3 = nn.Linear(256, 256)
         self.fc4 = nn.Linear(256, 1)
         self.dropout = nn.Dropout(p=0.2)
 
     def forward(self, x):
+        # TODO: NORM INPUT call at top of forward
         # Max pooling over a (2, 2) window
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-        x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
-        x = F.max_pool2d(F.relu(self.conv3(x)), (2, 2))   
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(F.relu(self.conv1a(x)), (2,2))
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(F.relu(self.conv2a(x)), (2, 2))
+        x = F.relu(self.conv3(x))
+        x = F.max_pool2d(F.relu(self.conv3a(x)), (2, 2))   
         x = x.view(-1, self.num_flat_features(x))
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
@@ -31,7 +36,7 @@ class Net(nn.Module):
         x = self.dropout(x)
         x = F.relu(self.fc3(x))
         x = self.fc4(x)
-        return x
+        return sigmoid(x)
 
     def num_flat_features(self, x):
         size = x.size()[1:]  # all dimensions except the batch dimension
