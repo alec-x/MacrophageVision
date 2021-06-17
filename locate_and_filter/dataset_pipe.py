d4ecdf72b76e11c5e72eab9e5e50e1669b5c7521
alec xu <alec_xu@hotmail.com>
2021-06-16 18:00:45 -0700

    .

--- locate_and_filter/dataset_pipe.py
+++ locate_and_filter/dataset_pipe.py
@@ -5,29 +5,29 @@ from os import remove
 import uuid
 
 # Using parse arg to input parameters seems to interfere with lower level argparse calls
-PATH = r"D:\\kerryn_tiffs\\tiff_dec_sorted"
-CLEAR = False
-
+PATH = r"D:\\bm_alveolar_sep_channel\\alveolar_macs_autofluorescent"
+CLEAR = True
+label = "alveolar"
 suffix = str(uuid.uuid4())
 
-located_path = "data\\interim\\located_" + "6ed6fa2e-fdbe-4959-be77-8c2ea0d8a0e5"
+located_path = "data\\interim\\located_" + suffix
 print("\nlocating cells")
 print("=================================================================")
-#locate_cells([PATH, "-o", located_path])
+locate_cells([PATH, "-o", located_path])
 print("=================================================================")
-
+    
 filtered_path = "data\\interim\\filtered_" + suffix 
 print("\nPerforming basic filtering")
 print("=================================================================")
 filter_basic([located_path, "-o", filtered_path])
 print("=================================================================")
 
-thresholded_path = "data\\processed\\thresholded_" + suffix 
+
+thresholded_path = "data\\processed\\" + "alveolar_autof" # suffix 
 print("\nPerforming thresholding into classes")
 print("=================================================================")
-thresholding([filtered_path, "-o", thresholded_path])
+thresholding([filtered_path, label, "-o", thresholded_path])
 print("=================================================================")
-
 
 if CLEAR:
     print("\nRemoving intermediary files")
