3ed7c888f0a333b662f424435b7063c9a6238249
alec xu <alec_xu@hotmail.com>
2021-06-16 17:59:37 -0700

    change location

--- locate_and_filter/dataset_label.py
+++ locate_and_filter/dataset_label.py
@@ -35,43 +35,44 @@ def main(raw_args=None):
 
     print("\nLoading pickle")
     raw_data = pickle.load(open(data_file, "rb"))
-
+    """
     print("\nApplying selection criteria")
     criterion = []
     criterion.append(raw_data['cd206_measure'] > 50) 
     criterion.append(raw_data['cd206_measure'] < 5)
-
+    
     data_selections = []
     for i, criteria in enumerate(criterion):
         selected = raw_data.loc[criteria][["bf_path", "mito_path"]]
         selected["label"] = i
         print("label:", i, "# samples:", len(selected))
         data_selections.append(selected)
-    
     processed = pd.concat(data_selections)
+    """
+    print(raw_data)
+    processed = raw_data["mito", "nuclear", "label"]
+    
 
     print("\nSaving to output directory")
     
     os.mkdir(args.o)
-
-    class_name = 0
             
     with open(args.o + '\\labels.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',', 
                            quotechar='|', quoting=csv.QUOTE_MINIMAL) 
-        writer.writerow(["bf_path", "mito_path", "label"])
+        writer.writerow(["bf_path", "nuclear", "label"])
         for row in processed.itertuples(index=False):
             
             in_bf    = args.path + "\\" + row[0]
-            in_mito  = args.path + "\\" + row[1]
+            in_nuclear  = args.path + "\\" + row[1]
             bf_file = row[0].split("\\")[-1]
-            mito_file =row[1].split("\\")[-1]
+            nuclear_file =row[1].split("\\")[-1]
             out_bf   = args.o + "\\" + bf_file
-            out_mito = args.o + "\\" + mito_file
+            out_nuclear = args.o + "\\" + nuclear_file
 
             shutil.copy(in_bf, out_bf)
-            shutil.copy(in_mito, out_mito)
-            writer.writerow([bf_file, mito_file, row[2]])
+            shutil.copy(in_nuclear, out_nuclear)
+            writer.writerow([bf_file, nuclear_file, row[2]])
             
 if __name__=="__main__":
     main()
