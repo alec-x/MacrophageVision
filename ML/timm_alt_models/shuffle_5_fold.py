5d41dd0005d48f2ad350286b34957ec9d4d130b8
alec xu <alec_xu@hotmail.com>
2022-02-16 23:34:52 -0800

    improved 5-fold script

--- ML/timm_alt_models/shuffle_5_fold.py
+++ ML/timm_alt_models/shuffle_5_fold.py
@@ -15,29 +15,51 @@ parser.add_argument('--k', default=5,
 def main(raw_args=None):
     args = parser.parse_args()
     if os.path.isdir(args.out):
+        print("deleting existing directory at out path")
         shutil.rmtree(args.out)
     
-    
-    for k in range(args.k):
-        os.makedirs(args.out + "/fold_" + str(k + 1))
     class_dirs = os.listdir(args.data)
-
+    
+    img_seq = {}
     for class_dir in class_dirs:
-        print("Processing Class " + class_dir)
+        img_seq[class_dir] = {}
         src_dir = args.data + '/' + class_dir
         files = os.listdir(src_dir)
-        num_images = len(files)
-        img_seq = random.sample(list(range(num_images)), k=num_images)
-        img_seq = np.array_split(img_seq, args.k)
+        num_img = len(files)
+        sequence = random.sample(list(range(num_img)), k=num_img) 
         
         for k in range(args.k):
-            print("Fold " + str(k + 1) + "...")
-            out_path = args.out + "/fold_" + str(k + 1) + "/" + class_dir
-            os.makedirs(out_path)
-            for i in img_seq[k]:
+            img_seq[class_dir][k] = {}
+            folds = list(range(args.k))
+            folds.remove(k)
+            split_seq = np.array_split(sequence, args.k) 
+
+            train = [list(split_seq[fold]) for fold in folds]
+            train = sum(train, [])
+            test = list(split_seq[k])
+
+            img_seq[class_dir][k]["train"] = train
+            img_seq[class_dir][k]["test"] = test
+
+    for k in range(args.k):
+        
+        out_path = args.out + "/fold_" + str(k + 1)
+        os.makedirs(out_path + "/train")
+        os.makedirs(out_path + "/test")
+
+        for class_dir in class_dirs:
+            print("Processing Fold " + str(k + 1) + " " + class_dir,end='\r')
+            src_dir = args.data + '/' + class_dir
+            os.makedirs(out_path + "/train/" + class_dir)
+            os.makedirs(out_path + "/test/" + class_dir)
+            files = os.listdir(src_dir)
+                
+            for i in img_seq[class_dir][k]["train"]:
                 src = src_dir + "/" + files[i]
-                shutil.copy(src, out_path)
-    
+                shutil.copy(src, out_path + "/train/" + class_dir)
+            for i in img_seq[class_dir][k]["test"]:
+                src = src_dir + "/" + files[i]
+                shutil.copy(src, out_path + "/test/" + class_dir)
     return
 
 if __name__ == '__main__':
